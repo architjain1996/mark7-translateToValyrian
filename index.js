8dcba492d8bd8a6ef1f5f3b49e6513be247babe5
architjain1996 <architjainudr.aj@gmail.com>
2021-09-24 02:45:48 +0530

    added 3 files

---
+++ index.js
@@ -0,0 +1,20 @@
+var inputId=document.querySelector("#textEnglish");
+
+var textMinionese=document.querySelector("#textMinionese");
+
+var btnTranslate=document.querySelector("#btnTranslate");
+
+function urlConstructor(inputText)
+{
+    return "https://api.funtranslations.com/translate/minion.json?text='"+inputText+"'";
+}
+
+function translateToMinionese()
+{
+    fetch(urlConstructor(inputId.value)).then(response=>response.json()).then(json => { textMinionese.innerText=json.contents.translated })
+}
+
+btnTranslate.addEventListener("click", function clickEventListener()
+{
+    fetch(urlConstructor(inputId.value)).then(response=>response.json()).then(json => { textMinionese.innerText=json.contents.translated })
+});
